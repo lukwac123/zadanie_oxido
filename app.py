966ff68ab99a9d90c9607fa49531056196b0e6f6
lukwac123 <lukasz-waclawek@wp.pl>
2024-11-15 21:07:08 +0100

    Utworzenie funkcji do zapisywania wyniku w pliku html.

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ import os
 def read_article(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+
+# Funkcja służąca do zapisania wynikowego kodu w pliku HTML.
+def save_html(content, file_path):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
